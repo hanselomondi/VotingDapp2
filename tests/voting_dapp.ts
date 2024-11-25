b665a860ca5e562c5fc25a1dd45715e740c99679
Hansel Omondi <ogopabhs@gmail.com>
2024-11-25 06:55:18 +0300

    Add: unit test for initialise_counters instruction

--- tests/voting_dapp.ts
+++ tests/voting_dapp.ts
@@ -1,6 +1,8 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { VotingDapp } from "../target/types/voting_dapp";
+import { PublicKey, SystemProgram, Transaction } from "@solana/web3.js";
+import { expect } from "chai";
 
 describe("voting_dapp", () => {
   // Configure the client to use the local cluster.
@@ -8,9 +10,34 @@ describe("voting_dapp", () => {
 
   const program = anchor.workspace.VotingDapp as Program<VotingDapp>;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
-  });
+  // Initialise the platform
+  it("Initialise", async () => {
+    let counter;
+    let registrations;
+
+    const [counterAddress] = PublicKey.findProgramAddressSync(
+      [Buffer.from("counter")],
+      program.programId
+    );
+    
+    const [registrationsAddress] = PublicKey.findProgramAddressSync(
+      [Buffer.from("registrations")],
+      program.programId
+    )
+
+    try {
+      counter = await program.account.counter.fetch(counterAddress);
+      registrations = await program.account.registrations.fetch(registrationsAddress);
+      console.log("Platform already initialised");
+      console.log(counter);
+      console.log(registrations);
+    } catch(err) {
+      console.log("Platform not initialised yet. Initialising...");
+      await program.methods.initialiseCounters().rpc();
+
+    }
+
+    expect(counter.count.toNumber()).equal(0);
+    expect(registrations.count.toNumber()).equal(0);
+  })
 });
