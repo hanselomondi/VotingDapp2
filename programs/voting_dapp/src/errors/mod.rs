37a381d736b07c70945e42893b8514bc05a3ef93
Hansel Omondi <ogopabhs@gmail.com>
2024-11-25 04:30:46 +0300

    Add: InvalidPollIdProvided error enum variant

--- programs/voting_dapp/src/errors/mod.rs
+++ programs/voting_dapp/src/errors/mod.rs
@@ -2,10 +2,12 @@ use anchor_lang::prelude::*;
 
 #[error_code]
 pub enum VotingDappErrors {
-    #[msg("The poll does not exist")]
+    #[msg("The poll does not exist or has not been initialised")]
     NonExistentPoll,
     #[msg("Poll end time must be greater than the start time")]
     InvalidEndTime,
     #[msg("Poll cannot end before current time")]
-    InvalidPollTime
+    InvalidPollTime,
+    #[msg("Invalid poll id provided")]
+    InvalidPollIdProvided
 }
